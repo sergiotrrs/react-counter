9e2804f3192fcb809889eb59132e8c7f0ff46e81
Sergio Torres <sergio.torres@idr.org.mx>
2023-01-29 01:03:21 -0600

    style: buttons container

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import '../styles/Buttons.css';
-import '../styles/Counter.css';
+import "../styles/Buttons.css";
+import "../styles/Counter.css";
 import { useState } from "react";
 import { ButtonGen } from "./ButtonGen";
 
@@ -12,19 +12,27 @@ export const Counter = ({ initialValue }) => {
     setNumber(0);
   };
   const incrementCounter = () => {
-    setNumber(number+1);
+    setNumber(number + 1);
   };
   const decrementCounter = () => {
-    setNumber(number-1);
+    setNumber(number - 1);
   };
 
   return (
     <>
       <h1>Counter</h1>
       <p className="counter">{number}</p>
-      <ButtonGen className="button-clic" onClick={incrementCounter}>+</ButtonGen>
-      <ButtonGen className="button-clic" onClick={decrementCounter}>-</ButtonGen>
-      <ButtonGen className="button-reset" onClick={resetCounter}>Reset</ButtonGen>
+      <div className="buttons-container">
+        <ButtonGen className="button-clic" onClick={incrementCounter}>
+          +
+        </ButtonGen>
+        <ButtonGen className="button-clic" onClick={decrementCounter}>
+          -
+        </ButtonGen>
+        <ButtonGen className="button-reset" onClick={resetCounter}>
+          Reset
+        </ButtonGen>
+      </div>
     </>
   );
 };
